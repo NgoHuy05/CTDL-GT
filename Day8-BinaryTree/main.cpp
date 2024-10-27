070363b3fb67175e221a1d37d63afbd874710a4a
NgoHuy05 <23020610@vnu.edu.vn>
2024-10-27 15:32:15 +0700

    Day8

--- Day8-BinaryTree/main.cpp
+++ Day8-BinaryTree/main.cpp
@@ -50,18 +50,63 @@ node *insert(node *root, int value) {
     return root;
 }
 
-int demLa(node *root) {
-    if (root == NULL) {
-        return 0;
-    }
-    if (root->left == NULL && root->right == NULL) {
-        return 1;
-    }
-    int cnt = 0;
-    cnt += demLa(root->right);
-    cnt += demLa(root->left);
-    return cnt;
+node* minNode(node* root) {
+    node* current = root;
+    while (current && current->left != nullptr)
+        current = current->left;
+    return current;
 }
+
+node* deleteNode(node* root, int key) {
+    if (root == nullptr) return root;
+    if (key < root->data) root->left = deleteNode(root->left, key);
+    else if (key > root->data) root->right = deleteNode(root->right, key);
+    else {
+        if (root->left == nullptr) {
+            node* temp = root->right;
+            delete root;
+            return temp;
+        } else if (root->right == nullptr) {
+            node* temp = root->left;
+            delete root;
+            return temp;
+        }
+        node* temp = minNode(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+
+int search(node *root, int key) {
+    if (root == NULL) return 0;
+    if (root->data == key) return 1;
+    else if (root->data < key) {
+        return search(root->left, key);
+    }
+    else if (root->data > key) {
+        return search(root->right, key);
+    }
+}
+
+int Height(node *root) {
+    if (root == NULL) return 0;
+    int left = Height(root->left);
+    int right = Height(root->right);
+    return max(left,right) + 1;
+}
+
+int countLeaves(node* root) {
+    if (root == nullptr) return 0;
+    if (root->left == nullptr && root->right == nullptr) return 1; 
+    return countLeaves(root->left) + countLeaves(root->right);
+}
+
+int countNodes(node* root) {
+    if (root == nullptr) return 0;
+    return 1 + countNodes(root->left) + countNodes(root->right);
+}
+
 int main() {
 
 }
